e559cd04a8e14c33498ec6827a677b401b57d2f4
Haron <62265393+HaronT@users.noreply.github.com>
2023-11-07 03:06:18 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const getChangedFilesCoverage = require("./services/changedFilesCoverage");
 const runCodeCoverage = async (coverage) => {
   const diff = await getBaseCoverageDiff(coverage);
   core.info(`Code coverage diff: ${diff}%`);
+  core.info(`Test Logging`);
 
   const changedFilesCoverage = await getChangedFilesCoverage(coverage);
   await sendSummaryComment(changedFilesCoverage, diff, coverage.percentage);
