45e6edae8b592361b8b2abf7a285fda5acdc4d59
Haron <62265393+HaronT@users.noreply.github.com>
2023-11-07 02:17:47 -0600

    Update changedFilesCoverage.js

--- src/services/changedFilesCoverage.js
+++ src/services/changedFilesCoverage.js
@@ -7,7 +7,10 @@ const { getWorkspacePath } = require("../input");
 const getChangedFilesCoverage = async (coverage) => {
   const pullRequestContext = getPullRequestContext();
 
-  if (!pullRequestContext) return coverage.data;
+  if (!pullRequestContext) {
+    core.info(`NO PullRequestContext`);
+    return coverage.data;
+  }
 
   const octokit = await getOctokit();
 
